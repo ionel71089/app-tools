e2e2cf19be0d01792d3e53fbf09bf05801bbdbac
Ionel Lescai <ionel.lescai@cgm.com>
2019-06-24 17:47:19 +0300

    no message

--- cmd/cmd/UserApp.swift
+++ cmd/cmd/UserApp.swift
@@ -6,8 +6,8 @@
 //  Copyright © 2019 Ionel Lescai. All rights reserved.
 //
 
-import Foundation
 import CommandLineKit
+import Foundation
 import Fuzi
 
 class UserApp {
@@ -23,42 +23,41 @@ class UserApp {
             version
         )
     }
-    
+
     func run() {
         do {
             try cli.parse()
-            
+
             if version.wasSet {
                 printVersion()
                 return
             }
-            
+
             let genders = ["male", "female"]
             let gender = genders.randomElement()!
-            
+
             let html = try String(contentsOf: URL(string: "https://www.fakenamegenerator.com/gen-\(gender)-us-za.php")!)
             let doc = try HTMLDocument(string: html, encoding: String.Encoding.utf8)
-            let name = doc.firstChild(xpath: "//*[@id=\"details\"]/div[2]/div[2]/div/div[1]/h3")?.stringValue ?? ""
-            let id = doc.firstChild(xpath: "//*[@id=\"details\"]/div[2]/div[2]/div/div[2]/dl[2]/dd/text()")?.stringValue ?? ""
-            let birthday = doc.firstChild(xpath: "//*[@id=\"details\"]/div[2]/div[2]/div/div[2]/dl[6]/dd/text()")?.stringValue ?? ""
-            
+
+            let name = doc.firstChild(xpath: "//*[@id=\"details\"]/div[2]/div[2]/div/div[1]/h3")!.stringValue
+            let id = doc.firstChild(xpath: "//*[@id=\"details\"]/div[2]/div[2]/div/div[2]/dl[2]/dd/text()")!.stringValue
+            let birthday = doc.firstChild(xpath: "//*[@id=\"details\"]/div[2]/div[2]/div/div[2]/dl[6]/dd/text()")!.stringValue
+
             let nameComponents = name.components(separatedBy: " ")
-            let firstName = nameComponents[0...1].joined(separator: " ")
+            let firstName = nameComponents[0 ... 1].joined(separator: " ")
             let lastName = nameComponents[2]
-            
+
             let df = DateFormatter()
             df.dateFormat = "MMMM dd, yyyy"
             let date = df.date(from: birthday)!
-            
             let st = df.string(from: date)
-            
+
             let json: [String: Any] = [
                 "firstName": firstName,
                 "lastName": lastName,
                 "id": id.trimmingCharacters(in: .whitespacesAndNewlines),
                 "birthday": st,
-                "gender": gender.uppercased()
-                
+                "gender": gender.uppercased(),
             ]
             print(jsonDebug(json))
         } catch {
@@ -66,19 +65,13 @@ class UserApp {
                 printVersion()
                 return
             }
-            
+
             cli.printUsage(error)
             exit(EX_USAGE)
         }
     }
-    
+
     private func printVersion() {
-//        print("banana")
-//        print("""
-//            \("\(appName) \(VERSION)".blue)
-//            \("   ".onBlack)\("   ".onYellow)\("   ".onGreen)\("   ".onWhite)\("   ".onRed)\("   ".onBlue)
-//            
-//            """
-//        )
+        print("\(appName) \(VERSION)")
     }
 }
